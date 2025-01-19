5911cea6e571c2975d329cf48a0d3e1a7f52d249
Tawan-ok <saiordday1@gmail.com>
2025-01-19 23:21:48 +0700

    add use client into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+"use client";
+
+import { Suspense } from 'react';
 import Search from './components/search';
 import Result from './components/result';
 
 export default function Home() {
     return (
-        <div>
-       <h1 style={{ fontSize: '2.5rem', color: '#ffcb05', textShadow: '2px 2px #3b4cca' ,maxWidth: '800px', margin: '0 auto', textAlign: 'center', padding: '20px'}}>Search Pokémon</h1>
+        <div style={{ maxWidth: '1200px', margin: '0 auto', textAlign: 'center', padding: '20px' }}>
+            <h1 style={{ fontSize: '2.5rem', color: '#ffcb05', textShadow: '2px 2px #3b4cca' }}>Search Pokémon</h1>
             <Search />
-            <Result />
+            <Suspense fallback={<p>Loading Pokémon...</p>}>
+                <Result />
+            </Suspense>
         </div>
     );
 }
